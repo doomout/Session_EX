e03193bdb4f3294fcf3463794df472ca6345557a
khg <winquake3@hanmail.net>
2023-03-28 02:21:27 +0900

    조회한 리스트 정보를 쿠키로 남기기

--- src/main/java/com/zerck/controller/TodoReadController.java
+++ src/main/java/com/zerck/controller/TodoReadController.java
@@ -6,6 +6,7 @@ import com.zerck.dto.TodoDTO;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,11 +22,61 @@ public class TodoReadController extends HttpServlet {
             Long tno = Long.parseLong(req.getParameter("tno"));
             TodoDTO todoDTO = todoService.get(tno);
 
+            //모델 담기
             req.setAttribute("dto", todoDTO);
+
+            //쿠키 찾기(쿠키 이름은 viewTodos 로 지정)
+            Cookie viewTodoCookie = findCookie(req.getCookies(), "viewTodos");
+            
+            // 쿠키 값을 저장
+            String todoListStr = viewTodoCookie.getValue();
+
+            boolean exist = false;
+
+            //쿠키 값이 있고, 인덱스가 0 이상이면.
+            if (todoListStr != null && todoListStr.indexOf(tno + "-") >= 0) {
+                exist = true; //true로 저장
+            }
+
+            log.info("exist: " + exist);
+
+            //값이 있다면~
+            if (!exist) {
+                todoListStr += tno + "-"; //예:1-2-3-4-
+                viewTodoCookie.setValue(todoListStr); //
+                viewTodoCookie.setMaxAge(60*60*24); //유효 기간은 24시간으로 설정
+                viewTodoCookie.setPath("/"); //쿠키 저장소는 /
+                resp.addCookie(viewTodoCookie);
+            }
             req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
         }catch (Exception e) {
+            e.printStackTrace();
             log.error(e.getMessage());
             throw new ServletException("read error");
         }
+    }
+
+    private Cookie findCookie(Cookie[] cookies, String cookieName) {
+        Cookie targetCookie = null;
+        
+        //쿠키가 있고, 쿠키의 기간이 남아 있는가?
+        if (cookies != null && cookies.length > 0) {
+            //있으면 반복 해서 
+            for (Cookie ck:cookies) {
+                //쿠키 이름과 일치하면 저장해~
+                if (ck.getName().equals(cookieName)) {
+                    targetCookie = ck;
+                    break;
+                }
+            }
+        }
+        
+        //쿠키가 없어?
+        if (targetCookie == null) {
+            targetCookie = new Cookie(cookieName, ""); //생성하고
+            targetCookie.setPath("/"); //경로는 상위
+            targetCookie.setMaxAge(60*60*24); //유효시간은 24시간
+        }
+        return targetCookie; //쿠키 전송~
     }
 }
