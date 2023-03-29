98a0076e20ea946c50ec516a17b75b5c1fb4e77c
khg <winquake3@hanmail.net>
2023-03-29 23:53:21 +0900

    리스너 구현

--- src/main/java/com/zerck/controller/TodoListController.java
+++ src/main/java/com/zerck/controller/TodoListController.java
@@ -4,6 +4,7 @@ import com.zerck.service.TodoService;
 import lombok.extern.log4j.Log4j2;
 import com.zerck.dto.TodoDTO;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +21,10 @@ public class TodoListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("todo list......................");
+
+        //이 코드로 프로그램 실행시에 W2를 appName 이라는 곳에 저장할 수 있다.
+        ServletContext servletContext = req.getServletContext();
+        log.info("appName: " + servletContext.getAttribute("appName"));
 
         try{
             List<TodoDTO> dtoList = todoService.listAll();
